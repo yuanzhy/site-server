4a69badb2b011b7b076e85be8c61724905f58b4a
yuanzhy <yuanzhy@thunisoft.com>
2018-04-03 13:33:39 +0800

    添加ribbon使用方法注释

--- service-blog/src/main/java/com/my/site/service/blog/controller/DemoController.java
+++ service-blog/src/main/java/com/my/site/service/blog/controller/DemoController.java
@@ -7,12 +7,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+
 @RestController
 public class DemoController {
     @Autowired
     private RestTemplate restTemplate;
 
     /**
+     * 使用了ribbon，客户端负载均衡
+     * 1. 在SpringBootApplication添加 @Bean @LoadBalance 返回RestTemplate
+     * 2. 注入restTemplate，调用getForObject等方法，http://服务名/rest名
      * testRemote
      * @return
      */
