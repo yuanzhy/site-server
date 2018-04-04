67fde18c51cba4a6c44918aaa44095ea4840dec8
529164645@qq.com <529164645@qq.com>
2018-04-04 15:16:02 +0800

    修改了一下pom

---
+++ service-blog/src/main/java/com/my/site/service/blog/remote/UserServiceDemo.java
@@ -0,0 +1,13 @@
+package com.my.site.service.blog.remote;
+
+/**
+ * feign 调用远程服务
+ * 1. 引入 spring-cloud-starter-openfeign(springCloud1.4+改用这个)  open这个暂时不知道怎么用
+ * 2. 入口类添加@EnableFeignClients
+ * @Author Administrator
+ * @Date 2018/4/4
+ */
+@Deprecated
+public interface UserServiceDemo {
+
+}
