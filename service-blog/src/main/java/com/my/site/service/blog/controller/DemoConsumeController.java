254ce6fa4dc8e03c46df0f73c8f737e2f5619f6a
yuanzhy <529164645@qq.com>
2018-04-06 13:43:21 +0800

    编写hystrix demo

--- service-blog/src/main/java/com/my/site/service/blog/controller/DemoConsumeController.java
+++ service-blog/src/main/java/com/my/site/service/blog/controller/DemoConsumeController.java
@@ -1,16 +1,18 @@
 package com.my.site.service.blog.controller;
 
+import com.my.site.service.blog.remote.UserServiceDemo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 @RestController
-public class DemoController {
+public class DemoConsumeController {
     @Autowired
     private RestTemplate restTemplate;
 
     /**
+     * 测试远程调用，不适用断路器方式
      * testRemote
      * @return
      */
@@ -19,4 +21,20 @@ public class DemoController {
         System.out.println("远程调用了service-user");
         return "blog-service remote call user-service: " + restTemplate.getForObject("http://service-user/test", String.class);
     }
+
+    /**
+     * 支持断路器的远程调用方式
+     */
+    @Autowired
+    private UserServiceDemo userService;
+    /**
+     * 测试远程调用断路器方式
+     * @return
+     */
+    @GetMapping(value="testRemoteFB")
+    public String testRemoteFB() {
+        System.out.println("远程调用了支持断路器方式的service-user");
+        return userService.userService();
+    }
+
 }
--- service-blog/src/main/java/com/my/site/service/blog/remote/UserServiceDemo.java
+++ service-blog/src/main/java/com/my/site/service/blog/remote/UserServiceDemo.java
@@ -1,13 +1,55 @@
 package com.my.site.service.blog.remote;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.command.AsyncResult;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.concurrent.Future;
+
 /**
  * feign 调用远程服务
- * 1. 引入 spring-cloud-starter-openfeign(springCloud1.4+改用这个)  open这个暂时不知道怎么用
- * 2. 入口类添加@EnableFeignClients
+ * 远程调用，使用断路器方式的本地service stub
  * @Author Administrator
  * @Date 2018/4/4
  */
-@Deprecated
-public interface UserServiceDemo {
+@Service
+public class UserServiceDemo {
 
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @HystrixCommand(fallbackMethod = "userServiceFallback")
+    public String userService() {
+        return restTemplate.getForObject("http://service-user/test", String.class);
+    }
+
+    /**
+     * 异步版本
+     * @return
+     */
+    @HystrixCommand(fallbackMethod = "userServiceFallback")
+//    @HystrixCommand(fallbackMethod = "userServiceFallback",
+//            ignoreExceptions = {RuntimeException.class}, // 忽略异常配置，此异常不走fallback
+//            groupKey = "testGroupName", // 组名 -> 线程池隔离默认按组名划分
+//            commandKey = "testCommandKey")
+    public Future<String> userServiceAsync() {
+        return new AsyncResult<String>() {
+            @Override
+            public String invoke() {
+                return restTemplate.getForObject("http://service-user/test", String.class);
+            }
+        };
+    }
+
+    /**
+     * 断路器fallback方法
+     * 该方法也可能通过网络数据库等请求而发成异常，也可以添加@HystrixCommand 并实现二级fallback函数，俗称服务降级
+     * @return
+     */
+    private String userServiceFallback(Throwable e) {
+        System.out.println("正常调用发生了异常: " + e.fillInStackTrace());
+        return "service user is not available, hystrix take effect";
+    }
 }
