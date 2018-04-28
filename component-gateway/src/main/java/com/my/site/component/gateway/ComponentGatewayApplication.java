757441a6e9afe118737890816b7ff5b4456c3573
yuanzhy <yuanzhy@thunisoft.com>
2018-04-28 16:01:46 +0800

    添加zuul路由配置

--- component-gateway/src/main/java/com/my/site/component/gateway/ComponentGatewayApplication.java
+++ component-gateway/src/main/java/com/my/site/component/gateway/ComponentGatewayApplication.java
@@ -3,9 +3,11 @@ package com.my.site.component.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableZuulProxy
 public class ComponentGatewayApplication {
 
 	public static void main(String[] args) {
