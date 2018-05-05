40ffccec0440c92884bbd23f3f3e7b152d1bc1e7
yuanzhy <529164645@qq.com>
2018-05-05 17:56:09 +0800

    重构了结构

---
+++ service/user/src/main/java/com/my/site/service/user/UserApplication.java
@@ -0,0 +1,15 @@
+package com.my.site.service.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@EnableEurekaClient
+public class UserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserApplication.class, args);
+	}
+}
