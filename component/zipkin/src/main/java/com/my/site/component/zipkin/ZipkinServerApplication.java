cf8da12160fb0711a078f8b7b5a7b8a0e1f2574a
yuanzhy <529164645@qq.com>
2018-05-14 12:44:08 +0800

    完成网关的sleuth监控

--- component/zipkin/src/main/java/com/my/site/component/zipkin/ZipkinServerApplication.java
+++ component/zipkin/src/main/java/com/my/site/component/zipkin/ZipkinServerApplication.java
@@ -2,28 +2,20 @@ package com.my.site.component.zipkin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.core.Ordered;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import zipkin.server.internal.EnableZipkinServer;
 
 @SpringBootApplication
 @EnableZipkinServer
-//@Configuration
-public class ZipkinServerApplication/* implements WebMvcConfigurer */{
+public class ZipkinServerApplication implements WebMvcConfigurer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ZipkinServerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ZipkinServerApplication.class, args);
+    }
 
-//    public void addViewControllers(ViewControllerRegistry registry) {
-//        registry.addViewController("/").setViewName( "redirect:/zipkin/" );
-//        registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
-//    }
+    public void addViewControllers(ViewControllerRegistry registry) {
+//        registry.addRedirectViewController("/", "/zipkin/");
+    }
 
 }
